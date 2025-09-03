61f4760066deb5ac40a4256af3a45992bb959f5a
Arnab Ganguly <aganguly01@gmail.com>
2025-09-03 11:21:54 -0400

    changes

--- sns-eventbridge-sqs/test/sns-eventbridge-sqs.test.ts
+++ sns-eventbridge-sqs/test/sns-eventbridge-sqs.test.ts
@@ -1,17 +1,97 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as SnsEventbridgeSqs from '../lib/sns-eventbridge-sqs-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import { SnsEventbridgeSqsStack } from '../lib/sns-eventbridge-sqs-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/sns-eventbridge-sqs-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new SnsEventbridgeSqs.SnsEventbridgeSqsStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('SnsEventbridgeSqsStack', () => {
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeAll(() => {
+    const app = new cdk.App();
+    const stack = new SnsEventbridgeSqsStack(app, 'TestStack');
+    template = Template.fromStack(stack);
+  });
+
+  test('SNS topic is created', () => {
+    template.hasResourceProperties('AWS::SNS::Topic', {
+      DisplayName: 'Source SNS Topic',
+    });
+  });
+
+  test('SQS queue is created', () => {
+    template.hasResourceProperties('AWS::SQS::Queue', {
+      QueueName: 'destination-queue',
+    });
+  });
+
+  test('Custom EventBus is created', () => {
+    template.hasResourceProperties('AWS::Events::EventBus', {
+      Name: 'CustomJsonTransformBus',
+    });
+  });
+  /*
+
+  test('Lambda has EventBus name in environment variables', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Environment: {
+        Variables: {
+          //EVENT_BUS_NAME: 'CustomJsonTransformBus',
+          EVENT_BUS_NAME: { Ref: Match.stringLikeRegexp('CustomJsonTransformBus.*') },
+        },
+      },
+      Runtime: 'nodejs22.x',
+    });
+  });
+ */
+
+/*
+
+  test('Lambda has EventBus name in environment variables', () => {
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Environment: {
+      Variables: {
+        //EVENT_BUS_NAME: Match.anyValue(),
+        EVENT_BUS_NAME: { Ref: Match.stringLikeRegexp('CustomJsonTransformBus.*') },
+      },
+    },
+  });
+});
+*/
+
+test('Lambda has EventBus name in environment variables', () => {
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Environment: {
+      Variables: {
+        EVENT_BUS_NAME: Match.objectLike({
+          Ref: Match.stringLikeRegexp('CustomEventBus.*'),
+        }),
+      },
+    },
+  });
+});
+
+test('EventBridge Rule forwards to SQS with correct JSON transformation', () => {
+  template.hasResourceProperties('AWS::Events::Rule', {
+    Name: 'JsonTransformAndForward',
+    EventPattern: {
+      source: ['custom.sns.source'],
+      detail: {
+      eventType: ['evMetadatgaCApture'],
+    },
+    },
+    Targets: Match.arrayWith([
+      Match.objectLike({
+        InputTransformer: {
+          InputPathsMap: {
+            'detail-decisionOutComeRequestId': '$.detail.decisionOutComeRequestId',
+            'detail-eventDataList-0--decisionOutComeId': '$.detail.eventDataList[0].decisionOutComeId',
+          },
+          InputTemplate: Match.stringLikeRegexp(
+            '.*<detail-decisionOutComeRequestId>.*<detail-eventDataList-0--decisionOutComeId>.*'
+          ),
+        },
+      }),
+    ]),
+  });
+});
+
 });
